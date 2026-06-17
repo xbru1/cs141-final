a6d336414da6be380912c34f3c98ac58f6c29d66
xbru1 <xbrumley.edu@gmail.com>
2026-06-16 17:22:58 -0700

    Add public keyword to Renderable

--- src/main/java/Renderable.java
+++ src/main/java/Renderable.java
@@ -6,7 +6,7 @@ import javafx.scene.*;
 import javafx.scene.image.*;
 import java.io.*;
 
-interface Renderable {
+public interface Renderable {
 	public void setSprite(String path, Group g) throws FileNotFoundException;
 	public void render();
 	public void initialize() throws FileNotFoundException;
